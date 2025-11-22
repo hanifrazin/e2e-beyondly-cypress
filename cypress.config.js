eeba683863a6d796ce824103a087de5bc31a0b7f
Hanif Razin Rahmatullah <hanifrazin@gmail.com>
2025-11-22 23:15:53 +0700

    automation flow login is success

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,35 @@
 const { defineConfig } = require("cypress");
 
+const envVariables = {
+  base_url: "https://recruitment-staging-queenbee.paradev.io",
+}
+
 module.exports = defineConfig({
+  viewportHeight: 1080,
+  viewportWidth: 1920,
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/reports/mochawesome-report',
+    overwrite: true,
+    html: true,
+    json: true,
+    timestamp: 'mmddyyyy_HHMMss'
+  },  
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      on('task', {
+        log(message) {
+          console.log(message)
+
+          return null
+        },
+      })
+    },
+    chromeWebSecurity: false,
+    defaultCommandTimeout: 60000,
+    env:{
+      ...envVariables
     },
   },
 });
--- cypress/e2e/e2e-beyondly.cy.js
+++ cypress/e2e/e2e-beyondly.cy.js
@@ -1,4 +1,5 @@
 describe('Automation E2E Beyondly Ecommerce', () => {
+    const base_url = Cypress.env('base_url');
     let dataUser = {};
 
     before(() => {
@@ -8,6 +9,21 @@ describe('Automation E2E Beyondly Ecommerce', () => {
         cy.fixture('data.json').then((data) => {
             dataUser = data;
         })
+    })
+
+    it('E2E process from login to checkout (excluding payment)', () => {
+        // --- LOGIN ---
+        cy.visit(base_url);
+        cy.contains('header p', 'Ketahui aroma yang cocok untuk kepribadianmu')
+            .parent()           
+            .find('svg')        
+            .click({ force: true });
+        cy.get('[href="\/login"]').click();
+        cy.url().should('eq',`${base_url}/login`);
+        cy.get('#page-login__tabs-number__input-number').clear().type(dataUser.nohp);
+        cy.get('#page-login__tabs-email__input-password').clear().type(dataUser.password);
+        cy.get('#page-login__button-login').click();
+        cy.url().should('eq',`${base_url}/home`);
     })
 
 
